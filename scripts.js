4f689def30f76b5dad383df610a293b8759fc670
Hannah-Lisa Whittle <whittlehannahlisa@gmail.com>
2021-10-24 19:46:41 -0500

    Added code to file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-/* Add your JavaScript to this file */
+window.onload = function () {
+   var form = document.querySelector("form");
+   form.setAttribute('onsubmit','return validateInput (event);');
+ }
+
+function validateInput(event) {
+   event.preventDefault();
+   let checkEmpty = document.querySelector("input");
+   let message = document.getElementsByClassName("message");
+   var error = "Please enter a valid email address";
+   var success = "Thank you! Your email address " + checkEmpty.value + " has been added to our mailing list!";
+
+   if(checkEmpty.value == ""){
+      alert(message.innerHTML = error);
+      return false;
+   }
+   alert(message.innerHTML = success);  
+}
