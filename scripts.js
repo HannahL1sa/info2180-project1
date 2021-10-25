1cbbf84a080b290a21aacc1e2e8c2b3395e8e868
Hannah-Lisa Whittle <whittlehannahlisa@gmail.com>
2021-10-24 20:07:51 -0500

    Added code to file

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ function validateInput(event) {
    event.preventDefault();
    let checkEmpty = document.querySelector("input");
    let message = document.getElementsByClassName("message");
-   var error = "Please enter a valid email address";
+   var error = "Please enter a valid email address.";
    var success = "Thank you! Your email address " + checkEmpty.value + " has been added to our mailing list!";
 
    if(checkEmpty.value == ""){
